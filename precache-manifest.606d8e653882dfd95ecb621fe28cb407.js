78611d08c1ef8accdefe5faf8333a2cba3b5107b
Muhammmad Usama Aleem <56190389+muhammad-usama-aleem@users.noreply.github.com>
2020-07-13 16:25:21 +0500

    Updates

---
+++ precache-manifest.606d8e653882dfd95ecb621fe28cb407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840e19b702805157a2ab718e00708446",
+    "url": "/react-router/index.html"
+  },
+  {
+    "revision": "728d10c4a8c52cb9c35f",
+    "url": "/react-router/static/css/main.09d0176e.chunk.css"
+  },
+  {
+    "revision": "57d8dfc5cb1dc62c6245",
+    "url": "/react-router/static/js/2.d23f6fed.chunk.js"
+  },
+  {
+    "revision": "bdcfdbef37b771299e3e4252a0d10375",
+    "url": "/react-router/static/js/2.d23f6fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728d10c4a8c52cb9c35f",
+    "url": "/react-router/static/js/main.cbdd8b58.chunk.js"
+  },
+  {
+    "revision": "743fec2fc5c21ae85d2d",
+    "url": "/react-router/static/js/runtime-main.c2221d3a.js"
+  }
+]);
